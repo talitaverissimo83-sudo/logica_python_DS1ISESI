000a0c6330f54165f6339f6f7c2d2d7504f48978
Talita <taita.verissimo.83@gmail.com>
2026-05-13 16:25:56 -0300

    atividade

---
+++ 1.1-exercio01.py
@@ -0,0 +1,34 @@
+# EX1
+idade = 10
+print(type(idade))
+
+# EX2
+temperatura = 23.5
+print("Valor:", temperatura)
+print("Tipo:", type(temperatura))
+
+# EX3 
+impedancia = 5 + 8j
+print(impedancia.real) 
+
+# EX4
+print(impedancia.imag)
+
+# EX5
+populacao = 8000000000
+print(type(populacao))
+
+# EX6
+
+print(type(7) == int)
+
+# EX7 
+aprovado = True
+print(type(aprovado))
+
+# EX8
+resultado = True + False
+print(resultado)
+print(type(resultado))
+
+# EX9
