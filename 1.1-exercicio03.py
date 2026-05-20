5e1d5403350cf7f4d35b00dd52961af0f4d21031
Talita <talita.verissimo.83@gmail.com>
2026-05-20 11:23:09 -0300

    Atividade 03-python

---
+++ 1.1-exercicio03.py
@@ -0,0 +1,74 @@
+# EX 01
+turma1 = 28
+turma2 = 32
+
+total_alunos = turma1 + turma2
+
+print("Total de alunos:", total_alunos)
+
+# EX 02
+pacote = 500
+baixado = 150
+
+restante = pacote - baixado
+
+print("Restam", restante, "MB")
+
+# EX 03
+ovos_por_caixa = 24
+caixas = 8
+
+total_ovos = ovos_por_caixa * caixas
+
+print("Total de ovos:", total_ovos)
+
+# EX 04
+suco = 10
+pessoas = 3
+
+resultado = suco / pessoas
+
+print("Cada pessoa receberá:", resultado, "litros")
+
+# EX 05
+litros = 10
+quantidade = 3
+
+resultado = litros // quantidade
+
+print("Pessoas que recebem 3 litros inteiros:", resultado)
+
+# EX 06
+resto = 10 % 3
+
+print("Resto da divisão:", resto)
+
+# EX 07
+potencia = 2 ** 8
+
+print("Resultado da potência:", potencia)
+
+# EX 8
+raiz = 81 ** 0.5
+
+print("Raiz quadrada de 81:", raiz)
+
+# EX 09
+expressao = (10 + 5) * 2 - 8 / 4
+
+print("Resultado da expressão:", expressao)
+
+# EX 10
+divisao_inteira = -7 // 2
+resto = -7 % 2
+
+print("Divisão inteira:", divisao_inteira)
+print("Resto da divisão:", resto)
+
+
+
+
+
+
+
+
