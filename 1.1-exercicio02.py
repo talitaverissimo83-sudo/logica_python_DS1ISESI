03039aae293bddb63a503dd8fedf1f964129579a
Talita <talita.verissimo.83@gmail.com>
2026-05-20 10:33:58 -0300

    Atividade 02-python

--- 1.1-exercicio02.py
+++ 1.1-exercicio02.py
@@ -1,2 +1,51 @@
 # EX 1
-# 
+idade = int("25")
+print(idade + 5)
+
+# EX 2
+numero = int(7.999)
+print(numero)
+
+# EX 3
+valor = float("-3.14")
+print(valor * 2)
+
+# EX 4
+# print(int("cento e vinte"))
+
+#Resultado:
+#ValueError: invalid literal for int() with base 10
+
+# EX 5
+
+texto = str(42)
+print(texto + " respostas")
+
+# EX 6
+numero_complexo = complex(3, 5)
+
+print("Número complexo:", numero_complexo)
+print("Parte real:", numero_complexo.real)
+print("Parte imaginária:", numero_complexo.imag)
+
+# EX 7
+print(bool(0))
+
+# EX 8
+print(bool(-100))
+
+# EX 9
+resultado = str(int(3.1415))
+
+print("Resultado da conversão:", resultado)
+print("Tipo do resultado:", type(resultado))
+
+# EX 10
+resultado = 5 + 2.3
+print(resultado)
+print(type(resultado))
+
+
+
+
+
